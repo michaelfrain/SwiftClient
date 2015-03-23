0f89994871e252559f317e457492db0e426ed91c
Michael Frain <michael.frain@hudl.com>
2015-03-23 16:14:25 -0500

    Initial Commit

---
+++ VideoServer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  VideoServer
+//
+//  Created by Michael Frain on 3/23/15.
+//  Copyright (c) 2015 Michael Frain. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
