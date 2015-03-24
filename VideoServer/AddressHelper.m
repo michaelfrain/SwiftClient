01e121538a46c2ad39f964263d58d0e893c6f73b
Michael Frain <michael.frain@hudl.com>
2015-03-24 02:02:15 -0500

    first commit

---
+++ VideoServer/AddressHelper.m
@@ -0,0 +1,13 @@
+//
+//  AddressHelper.m
+//  VideoServer
+//
+//  Created by Michael Frain on 3/23/15.
+//  Copyright (c) 2015 Michael Frain. All rights reserved.
+//
+
+#import "AddressHelper.h"
+
+@implementation AddressHelper
+
+@end
---
+++ VideoServer/ObjcServerConnection.m
@@ -0,0 +1,13 @@
+//
+//  ObjcServerConnection.m
+//  VideoServer
+//
+//  Created by Michael Frain on 3/23/15.
+//  Copyright (c) 2015 Michael Frain. All rights reserved.
+//
+
+#import "ObjcServerConnection.h"
+
+@implementation ObjcServerConnection
+
+@end
